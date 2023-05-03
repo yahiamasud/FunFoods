3b7a695af78be7b9531c70050eb9cce129d3ffcc
Yahia Masud <79189930+yahiamasud@users.noreply.github.com>
2023-05-03 22:37:31 +0600

    chef all card eidit

--- src/Compounent/Items/Items.jsx
+++ src/Compounent/Items/Items.jsx
@@ -3,15 +3,16 @@ import './Items.css'
 import { Link } from 'react-router-dom';
 
 const Items = ({chefdata}) => {
-    const {name, id,photo } = chefdata ;
+    const {name, position ,photo,start_date} = chefdata ;
     return (
-        <div className='  w-50 my-10'>
+        <div className='itemscard  w-50 my-10'>
             <div className="card  glass">
                 <figure><img className='imgItems' src={photo} alt="car!" /></figure>
                 <div className="card-body">
-                    <h2 className="card-title">Name:{name}</h2>
-                    <h2>position:{}</h2>
-                    <p>How to park your car at your garage?</p>
+                    <h2 className="card-title">Name: {name}</h2>
+                    <h2 className='text-xl'>Position: {position}</h2>
+                    <p>{start_date}</p>
+                    <p> Corrupti perferendis modi quo eligendi illum exercitationem molestiae magnam voluptatum dolore dignissimos? Blanditiis voluptate esse facere, provident in unde officiis eligendi modi.</p>
                     <div className="card-actions justify-end">
                     <button className="btn btn-primary"><Link to={"/Datials"}>View Details</Link></button>
                     </div>
