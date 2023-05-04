78d8c7ceb020035558cc066d819758649e2f6f32
Yahia Masud <79189930+yahiamasud@users.noreply.github.com>
2023-05-04 14:11:46 +0600

    logoin profile img show

--- src/Compounent/Header/Header.jsx
+++ src/Compounent/Header/Header.jsx
@@ -1,15 +1,17 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../../providers/AuthProviders';
+import './head.css'
 
 const Header = () => {
-    const {user,logOut }=useContext(AuthContext)
-    const handleLogout=()=>{
+    const { user, logOut } = useContext(AuthContext)
+    const handleLogout = () => {
         logOut()
-        .then()
+            .then()
 
     }
-   
+    console.log(user);
+
     return (
         <div className="navbar mt-2 bg-base-200">
             <div className="flex-1">
@@ -23,13 +25,13 @@ const Header = () => {
                 </ul>
                 <div className='flex ml-5 px-5'>
                     {
-                        user &&  <span className='mx-3'>{user.name}</span>
+                        user && <span className='mx-3'><img className="imgProfile  " src={user.photoURL} alt=""/></span>
                     }
-                   { user ? <button onClick={handleLogout} className='font-bold'>Logout</button>:
-                    <Link to="/Login">
-                    <button className='font-bold'>Login</button>
-                    </Link>
-                   }
+                    {user ? <button onClick={handleLogout} className='font-bold'>Logout</button> :
+                        <Link to="/Login">
+                            <button className='font-bold'>Login</button>
+                        </Link>
+                    }
                 </div>
             </div>
         </div>
