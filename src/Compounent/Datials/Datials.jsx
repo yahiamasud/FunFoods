a0404d784a49fe7d22111770be756477391b5bac
Yahia Masud <79189930+yahiamasud@users.noreply.github.com>
2023-05-05 21:14:01 +0600

    all compled

--- src/Compounent/Datials/Datials.jsx
+++ src/Compounent/Datials/Datials.jsx
@@ -1,33 +1,40 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import './daa.css';
+import { AuthContext } from '../../providers/AuthProviders';
 
 const Datials = () => {
     const [items, setItems] = useState([])
+    const {loading }= useContext(AuthContext)
     const { id } = useParams();
-    // const{ name }= items.items;
+    
     useEffect(() => {
         fetch(`https://assingmen-yahiamasud.vercel.app/chefdata/${id}`)
             .then((res) => res.json())
             .then((data) => {
-                console.log(data)
+                // console.log(data)
                 setItems(data)
             });
     }, [id]);
-    console.log();
+    if(!items){
+        return loading
+    }
+  
     return (
         <div className='container'>
             <div className=" bg-base-100 shadow-xl">
             
-                <img className='detialphoto' src="" alt="" />
+                <img className='detialphoto' src={items?.items?.photo} alt="" />
                 <div className="card-body">
-                    <h2 className="card-title">name:-</h2>
-                    <h2 className='card-title'>Position:-</h2>
-                    <h2 className='card-title'>Company:-</h2>
-                    <h2 className='card-title'>Datials:-</h2>
+                    <h2 className="card-title">name {items?.items?.name }</h2>
+
+                    <h2 className='card-title'>Position:-{items?.items?.position}</h2>
+                    <h2 className='card-title'>Company:-{items?.items?.company}</h2>
+                    <h2 className='card-title'>Experince:-7year</h2>
+                    <h2 className='card-title'>Datials:-{items?.items?.dates}</h2>
                     <p></p>
                     <div className="card-actions justify-end">
-                        <button className="btn btn-primary"><Link to="/Home">back</Link></button>
+                        <button className="btn btn-primary"><Link to="/">back</Link></button>
                     </div>
                 </div>
             </div>
